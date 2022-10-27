202d4a886f955694739ff392c581b9e39f834b46
PauloRev <paulohenriquerev@gmail.com>
2022-10-26 21:54:51 -0300

    Fix .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
